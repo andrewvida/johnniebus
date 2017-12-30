d80ff45d6dac8613d2325883c7a45ca90c1a88fa
Andrew Vida <andrewjvida@gmail.com>
2017-12-30 15:44:28 -0500

    working hard coded google map

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener("turbolinks:load", function () {
+  function initMap() {
+    // Create a map object and specify the DOM element for display.
+    var johnstown = { lat: 40.1536742, lng: -82.6851699 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: johnstown
+    });
+    var marker = new google.maps.Marker({
+      position: johnstown,
+      map: map
+    });
+  }
+
+  google.maps.event.addDomListener(window, 'load', initMap);
+
+});
