c40261dc52e99b79fcdb8042ffd980dc4b57a0f6
Andrew Vida <andrewjvida@gmail.com>
2017-12-31 18:33:34 -0500

    Set up basic data for dev

--- db/seeds/dev.rb
+++ db/seeds/dev.rb
@@ -1,6 +1,11 @@
+p "Seeding the dev database...."
 User.destroy_all
 Bus.destroy_all
 Location.destroy_all
 
 bus = Bus.create(number: 4)
-User.create(email: 'andy@vidalitycodeworks.com', bus_id: bus.id)
+user = User.create(email: 'andy@vcw.com', password: "password", password_confirmation: "password")
+Roster.create(bus_id: bus.id, user_id: user.id)
+
+p "Added #{Bus.all.count} busses and #{User.all.count} users."
+p "Seeding complete."
